e870ca2ea337fcc25af2b8250083d43bcbd86667
Jean Raphael Bordet <jean.bordet@deltatre.it>
2016-11-10 08:07:11 +0100

    Introduce geometry operations for UIImageView

--- UIImageView+Geometry.m
+++ UIImageView+Geometry.m
@@ -16,4 +16,29 @@
     self.layer.borderWidth = 0;
 }
 
+- (void)imageWithWithImageName:(NSString *)imageName height:(int)height {
+    UIImage *image = [UIImage imageWithContentsOfFile:imageName];
+    
+    if (image.size.height > height) {
+        NSData *imageData = UIImagePNGRepresentation(image);
+        
+        self.image = [UIImage imageWithData:imageData
+                                scale:[self scaleFactor:imageData
+                                                 height:height]];
+    }
+}
+
+- (float)scaleFactor:(NSData *)imageData height:(int)height {
+    UIImage *image = [UIImage imageWithData:imageData];
+    
+    float widthScaled = (image.size.width / image.size.height) * (height);
+    
+    float scaleFactor = (image.size.width / (widthScaled));
+    return scaleFactor;
+}
+
+- (void)centerWithSize:(CGSize)frame {
+     self.center = CGPointMake(frame.width / 2, frame.height / 2);
+}
+
 @end
