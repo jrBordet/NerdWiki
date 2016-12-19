b1dd16578d76fccea0e00d24e830d96f00e0d0d5
Jean Raphael Bordet <jean.bordet@deltatre.it>
2016-12-19 08:15:53 +0100

    Introduce NWTheme category

---
+++ NerdWiki/UIColor+NWTheme.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+NWTheme.h
+//  NerdWiki
+//
+//  Created by Jean Raphael Bordet on 16/12/2016.
+//  Copyright © 2016 Jean Raphael Bordet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (NWTheme)
+
++ (UIColor *)bgColor;
+
+@end
---
+++ NerdWiki/UIColor+NWTheme.m
@@ -0,0 +1,20 @@
+//
+//  UIColor+NWTheme.m
+//  NerdWiki
+//
+//  Created by Jean Raphael Bordet on 16/12/2016.
+//  Copyright © 2016 Jean Raphael Bordet. All rights reserved.
+//
+
+#import "UIColor+NWTheme.h"
+
+@implementation UIColor (NWTheme)
+
++ (UIColor *)bgColor {
+    return [self colorWithRed:32.0f/255.0f
+                       green:32.0f/255.0f
+                        blue:32.0f/255.0f
+                       alpha:1.0f];
+}
+
+@end
